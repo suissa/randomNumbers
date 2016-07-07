d7c27701beed3a9151f208b9eec1dd6a84cc8676
Christian Benseler <cbenseler@localhost.localdomain>
2016-07-07 11:47:01 -0300

    Check if paramaters are numbers

--- randomNumbers.js
+++ randomNumbers.js
@@ -1,5 +1,6 @@
 function randomNumber(start, end) {
-  const min = start || 1;
+
+  const min = (start && !isNan(start)) ? start : 1;
   const max = end || 100;
   return Math.floor(Math.random() * (max - min)) + min;
 }
